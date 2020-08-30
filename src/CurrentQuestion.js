4bf9cde82e1d8913691821e9b4daa2e54ac7878f
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-08-30 23:01:44 +0300

    feat: finish with styles for audio player for current bird

--- src/CurrentQuestion.js
+++ src/CurrentQuestion.js
@@ -15,7 +15,7 @@ class CurrentQuestion extends Component {
           <img className="CurrentQuestion-logo" src={props.isAnsweredCorrect ? correctSetOfData.image : defaultBirdImage} alt="Default bird"/>
           <div className="CurrentQuestion-control-panel">
             <div className="CurrentQuestion-bird-name">{props.isAnsweredCorrect ? correctSetOfData.name : '******'}</div>
-            <AudioPlayer layout="horizontal-reverse" autoPlayAfterSrcChange={false} showJumpControls={false} showDownloadProgress={false} src={correctSetOfData.audio} customControlsSection={[RHAP_UI.MAIN_CONTROLS, RHAP_UI.VOLUME_CONTROLS]}/>
+            <AudioPlayer className="AudioPlayer-column" layout="horizontal-reverse" autoPlayAfterSrcChange={false} showJumpControls={false} showDownloadProgress={false} src={correctSetOfData.audio} customControlsSection={[RHAP_UI.MAIN_CONTROLS, RHAP_UI.VOLUME_CONTROLS]}/>
           </div>
         </div>
       );
